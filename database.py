1701e00b6e73edf6c06b28dc0e83f7bec2b2e560
solarezz <122013283+solarezz@users.noreply.github.com>
2024-08-06 16:10:27 +0500

    version 0.4

--- database.py
+++ database.py
@@ -2,13 +2,42 @@ import aiosqlite
 
 
 class Database:
-    def __init__(self, db_name=r'C:\Users\solarezz\Desktop\database.db'):
+    def __init__(self, db_name='database.db'):
         self.db_name = db_name
 
-    async def add_user(self, user_id, name):
+    async def add_user(self, user_id_telegram, firstname, username):
         async with aiosqlite.connect(self.db_name) as db:
-            await db.execute('INSERT OR IGNORE INTO users (user_id, name) VALUES (?, ?)', (user_id, name))
+            await db.execute('INSERT OR IGNORE INTO users (user_id_telegram, firstname, username) VALUES (?, ?, ?)',
+                             (user_id_telegram,
+                              firstname,
+                              username))
             await db.commit()
+
+    async def update_discord_id(self, user_id_ds, user_id_tg):
+        async with aiosqlite.connect(self.db_name) as db:
+            await db.execute('UPDATE users SET user_id_discord = ? WHERE user_id_telegram = ?',
+                             (user_id_ds,
+                              user_id_tg))
+            await db.commit()
+
+    async def full_info_user(self, user_id_tg):
+        async with aiosqlite.connect(self.db_name) as db:
+            async with db.execute('SELECT * FROM users WHERE user_id_telegram = ?', (user_id_tg,)) as cursor:
+                info = await cursor.fetchone()
+                return info
+
+    async def update_cooldown_tg(self, cooldown_tg, user_id_tg):
+        async with aiosqlite.connect(self.db_name) as db:
+            await db.execute('UPDATE users SET cooldown_telegram = ? WHERE user_id_telegram = ?',
+                             (cooldown_tg,
+                              user_id_tg))
+            await db.commit()
+
+    async def info_cooldown_tg(self, user_id_tg):
+        async with aiosqlite.connect(self.db_name) as db:
+            async with db.execute('SELECT cooldown_telegram FROM users WHERE user_id_telegram = ?', (user_id_tg,)) as cursor:
+                info_cooldown = await cursor.fetchone()
+                return info_cooldown[0]
 
     async def info(self):
         async with aiosqlite.connect(self.db_name) as db:
@@ -16,30 +45,21 @@ class Database:
                 users = await cursor.fetchall()
                 return users
 
-    async def info_user(self, user_id):
-        async with aiosqlite.connect(self.db_name) as db:
-            async with db.execute('SELECT name FROM users WHERE user_id = ?', (user_id,)) as cursor:
-                info_user = await cursor.fetchone()
-                return info_user
-
     async def update_cooldown_ds(self, cooldown, user_id_ds):
         async with aiosqlite.connect(self.db_name) as db:
-            await db.execute('UPDATE users SET cooldown = ? WHERE user_id_ds = ?', (cooldown, user_id_ds))
+            await db.execute('UPDATE users SET cooldown_discord = ? WHERE user_id_discord = ?',
+                             (cooldown, user_id_ds))
             await db.commit()
 
     async def info_cooldown_ds(self, user_id_ds):
         async with aiosqlite.connect(self.db_name) as db:
-            async with db.execute('SELECT cooldown FROM users WHERE user_id_ds = ?', (user_id_ds,)) as cursor:
+            async with db.execute('SELECT cooldown_discord FROM users WHERE user_id_discord = ?',
+                                  (user_id_ds,)) as cursor:
                 info_cooldown = await cursor.fetchone()
                 return info_cooldown[0]
 
-    async def update_cooldown_tg(self, cooldown, user_id):
+    async def info_id(self, name):
         async with aiosqlite.connect(self.db_name) as db:
-            await db.execute('UPDATE users SET cooldown_tg = ? WHERE user_id = ?', (cooldown, user_id))
-            await db.commit()
-
-    async def info_cooldown_tg(self, user_id):
-        async with aiosqlite.connect(self.db_name) as db:
-            async with db.execute('SELECT cooldown_tg FROM users WHERE user_id = ?', (user_id,)) as cursor:
-                info_cooldown = await cursor.fetchone()
-                return info_cooldown[0]
+            async with db.execute('SELECT user_id_telegram FROM users WHERE firstname = ?', (name,)) as cursor:
+                info_id = await cursor.fetchone()
+                return info_id[0]
--- discordBot.py
+++ discordBot.py
@@ -1,103 +1,187 @@
 import disnake
 import asyncio
 import logging
+from aiogram.dispatcher.filters.state import StatesGroup, State
 from disnake.ext import commands
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from database import Database
 from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
 
-token = ''
+
+ds_token = ''
+
+tg_token = ''
 
 intents = disnake.Intents.default().all()
 
 db = Database()
 
-ID_CHANNEL = 875772759806984234
+ID_CHANNEL = 1059414521422290988
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = ''
+tg = Bot(token=tg_token)
+dp = Dispatcher(tg, storage=MemoryStorage())
 
-bottg = Bot(token=API_TOKEN)
-dp = Dispatcher(bottg, storage=MemoryStorage())
+ds = commands.Bot(command_prefix='/', intents=intents)
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+
+class Form(StatesGroup):
+    waiting_for_discord_id = State()
 
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await bottg.send_message(message.chat.id, "[✅] Вы успешно внесли свои данные в бота!")
-    await db.add_user(user_id=message.chat.id, name=message.from_user.first_name)
+    info = await db.full_info_user(message.chat.id)
+    if not info:
+        kb = [
+            [
+                types.KeyboardButton(text="👔 Привязать дискорд")
+            ]
+        ]
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=kb)
+        await tg.send_message(message.chat.id, "[✅] Вы успешно внесли свои данные в бота!", reply_markup=markup)
+        await db.add_user(user_id_telegram=message.chat.id,
+                          firstname=message.from_user.first_name,
+                          username=message.from_user.username)
+    elif not info[1]:
+        kb = [
+            [
+                types.KeyboardButton(text="👔 Привязать дискорд")
+            ]
+        ]
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=kb)
+        await tg.send_message(message.chat.id, "[✍] Привяжите свой дискорд!", reply_markup=markup)
+    else:
+        await tg.send_message(message.chat.id, '[👌] Вы уже зарегистрированы!')
+
+
+@dp.message_handler(lambda msg: msg.text.startswith('👔 Привязать дискорд'))
+async def input_id_discord(message: types.Message):
+    await Form.waiting_for_discord_id.set()
+    await message.answer("Что бы узнать свой Discord ID, введите в Discord команду /myid")
+    await message.reply('Пожалуйста введите свой Discord ID:')
+
+
+@dp.message_handler(commands=['discord'])
+async def input_id_discord(message: types.Message):
+    await Form.waiting_for_discord_id.set()
+    await message.answer("Что бы узнать свой Discord ID, введите в Discord команду /myid")
+    await message.reply('Пожалуйста введите свой Discord ID:')
+
+
+@dp.message_handler(state=Form.waiting_for_discord_id)
+async def process_discord_id(message: types.Message, state: FSMContext):
+    discord_id = message.text
+    try:
+        discord_int = int(discord_id)
+        await db.update_discord_id(user_id_tg=message.chat.id,
+                                   user_id_ds=discord_id)
+        markup = types.ReplyKeyboardRemove()
+        await message.reply(
+            f"Ваш Discord ID {discord_id} успешно привязан!\nЕсли вдруг вы случайно ввели не тот ID, напишите команду /discord", reply_markup=markup)
+        await state.finish()
+    except ValueError:
+        await message.reply("Пожалуйста, введите корректный Discord ID (число).")
 
 
 @dp.message_handler()
 async def message_in_discord(message: types.Message):
     cooldown = 15
-    info_cd = await db.info_cooldown_tg(user_id=message.chat.id)
+    info_cd = await db.info_cooldown_tg(user_id_tg=message.chat.id)
     if info_cd == 0:
         user_text = message.text
         await on_ready(name=message.from_user.first_name, message=user_text)
-        await db.update_cooldown_tg(cooldown=cooldown, user_id=message.chat.id)
+        await db.update_cooldown_tg(cooldown_tg=cooldown, user_id_tg=message.chat.id)
         asyncio.create_task(handle_cooldown_tg(message.chat.id, cooldown))
     else:
         await message.answer(f"[❌] Вы сможете отправлять команды через {info_cd} секунд!")
+
 
 async def handle_cooldown_tg(user_id, cooldown):
     while cooldown > 0:
         await asyncio.sleep(1)  # Ждем 1 секунду
         cooldown -= 1
-        await db.update_cooldown_tg(cooldown=cooldown, user_id=user_id)
-    await bottg.send_message(user_id, "[✅] Вы вновь можете отправлять сообщения в дискорд!")
+        await db.update_cooldown_tg(cooldown_tg=cooldown, user_id_tg=user_id)
+    await tg.send_message(user_id, "[✅] Вы вновь можете отправлять сообщения в дискорд!")
 
-@bot.event
+
+@ds.event
 async def on_ready(name, message):
-    try:
-        embed = disnake.Embed(title=f"[📩] Сообщение из телеграмм от {name}", color=0x5da6e8)
-        embed.add_field(name='[📝] Содержание сообщение:', value=message)
-        channel = bot.get_channel(ID_CHANNEL)
-        await channel.send(embed=embed)
-    except TypeError:
-        print(f'We have logged in as {bot.user}')
+    embed = disnake.Embed(title=f"[📩] Сообщение из телеграмм от {name}", color=disnake.Colour.blue())
+    embed.add_field(name='[📝] Содержание сообщение:', value=message)
+    embed.set_thumbnail(url="https://cdn2.iconfinder.com/data/icons/round-set-vol-2/120/sending-1024.png")
+    channel = ds.get_channel(ID_CHANNEL)
+    await channel.send(embed=embed)
 
 
-@bot.slash_command(name='info', description="Информация зарегистрированных пользователей в телеграмм")
+@ds.slash_command(name="myid", description='Информация о ID пользователя')
+async def myid(interaction: disnake.ApplicationCommandInteraction):
+    await interaction.send(f'Ваш ID: {interaction.author.id}', ephemeral=True)
+
+
+@ds.slash_command(name='info', description="Информация зарегистрированных пользователей в телеграмм")
 async def info(interaction: disnake.ApplicationCommandInteraction):
     users = await db.info()
-    user_list = '\n'.join([f'{user[0]} - {user[1]}' for user in users])
-    embed = disnake.Embed(title=f'[🌐] Пользователи которым вы можете отправить сообщение:\n', color=0x00ff00)
-    embed.add_field(name='[🆔] ID в Телеграмм - [🟣] Имя в Телеграмм', value=user_list)
+    user_list = '\n'.join([f'{user[2]} - @{user[3]}' for user in users])
+    embed = disnake.Embed(title=f'[🌐] Пользователи которым вы можете отправить сообщение:\n',
+                          color=disnake.Colour.blurple())
+    embed.add_field(name='', value=user_list)
+    embed.add_field(name="Как отправить сообщение?",
+                    value="Ввести команду /stg -> выбрать кому отправить -> написать сообщение",
+                    inline=False)
+    embed.set_thumbnail(url="https://cdn2.iconfinder.com/data/icons/round-set-vol-2/120/sending-1024.png")
     await interaction.send(embed=embed, ephemeral=True)
-    check = await db.info_cooldown_ds(user_id_ds=interaction.author.id)
 
 
-@bot.slash_command(name='send_tg', description="Отправить сообщение в телеграмм")
-async def send_tg(interaction: disnake.ApplicationCommandInteraction, user_id, message: str):
-    cooldown = 15
-    info_cd = await db.info_cooldown_ds(user_id_ds=interaction.author.id)
-    if info_cd == 0:
-        user = interaction.user
-        user_info = await db.info_user(user_id)
-        await interaction.send(f"[📨] Вы отправили сообщение пользователю - {user_info[0]}!")
-        await bottg.send_message(user_id, f'[{message}] - от {user.name}')
-        await db.update_cooldown_ds(cooldown=cooldown, user_id_ds=interaction.author.id)
-        asyncio.create_task(handle_cooldown(interaction.author.id, cooldown))
-    else:
-        await interaction.send(f"[❌] Вы сможете отправлять команды через {info_cd} секунд!")
+@ds.slash_command(name='stg', description="Отправить сообщение в телеграмм")
+async def stg(interaction: disnake.ApplicationCommandInteraction, name: str, message: str):
+    try:
+        cooldown = 15
+        info_cd = await db.info_cooldown_ds(user_id_ds=interaction.author.id)
+        if info_cd == 0:
+            users = await db.info()
+            user_list = [f'{user[2]}' for user in users]
+            if name in user_list:
+                user = interaction.user
+                user_id = await db.info_id(name)
+                await interaction.send(f"[📨] Вы отправили сообщение пользователю - {name}!")
+                await tg.send_message(user_id, f'[{message}] - от {user.name}')
+                await db.update_cooldown_ds(cooldown=cooldown, user_id_ds=interaction.author.id)
+                asyncio.create_task(handle_cooldown(interaction.author.id, cooldown))
+            else:
+                await interaction.send(f"[❌] Вы выбрали не существующего человека!")
+        else:
+            await interaction.send(f"[❌] Вы сможете отправлять команды через {info_cd} секунд!")
+    except:
+        await interaction.send("[❌] Вы не привязали Discord ID к Telegram боту!")
+
+
+@stg.autocomplete('name')
+async def name_autocomplete(interaction: disnake.ApplicationCommandInteraction, current: str):
+    users = await db.info()
+    user_list = [f'{user[2]}' for user in users]
+    choices = [name for name in user_list if current.lower() in name.lower()]
+    return choices
+
 
 async def handle_cooldown(user_id_ds, cooldown):
     while cooldown > 0:
-        await asyncio.sleep(1)  # Ждем 1 секунду
+        await asyncio.sleep(1)
         cooldown -= 1
         await db.update_cooldown_ds(cooldown=cooldown, user_id_ds=user_id_ds)
-    user = bot.get_user(user_id_ds)
+    user = ds.get_user(user_id_ds)
     await user.send("[✅] Вы вновь можете отправлять сообщения в телеграмм!")
 
-@bot.slash_command(name="dev", description="Разработчик бота")
+
+@ds.slash_command(name="dev", description="Разработчик бота")
 async def dev(interaction: disnake.ApplicationCommandInteraction):
     embed = disnake.Embed(title="[👨🏻‍💻] О боте:", color=0x185200)
     embed.add_field(name="[🛠] Разработчик", value="@solarezzwhynot")
-    embed.add_field(name="[⚙️] Версия", value="0.3")
+    embed.add_field(name="[⚙️] Версия", value="0.4")
     embed.add_field(name="[💳] Поддержка копеечкой для хостинга", value="2200 7007 1699 4750")
+    embed.set_thumbnail(url="https://i.pinimg.com/originals/f8/d0/bc/f8d0bc025046ab637a78a09598b905a7.png")
     await interaction.send(embed=embed)
 
 
@@ -110,4 +194,4 @@ async def main():
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.create_task(main())
-    loop.run_until_complete(bot.start(token))
+    loop.run_until_complete(ds.start(ds_token))
